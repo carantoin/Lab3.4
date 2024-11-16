ab8bfdc365512135a65c12398be307f90633823d
carantoin <90127329+carantoin@users.noreply.github.com>
2024-11-16 09:56:27 +0200

    Добавьте файлы проекта.

---
+++ Lab3.4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("┌---------------------------------------┐");
+Console.WriteLine($"|        x          |         y         |");
+Console.WriteLine("|---------------------------------------|");
+for (double x = -2; x <= 3; x = +0.2)
+{
+    double y;
+    if (x > 1.5) y = 2 * Math.Pow(x, 3) - 30;
+    else if (x >= 0 && x <= 1.5) y = x - 2;
+    else y = x * x * x * x * x;
+    Console.WriteLine($"|       {x,-5:F2}       |       {y,5:F2}       |");
+}
+Console.WriteLine("└---------------------------------------┘");
